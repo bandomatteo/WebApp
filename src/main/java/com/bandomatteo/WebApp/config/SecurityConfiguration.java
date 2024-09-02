c367df79d8a65dfb25801df71f979a064447a91c
Matteo Bando <bandomatteo@gmail.com>
2024-09-02 16:31:19 +0200

    chore: Update SecurityConfiguration to remove unnecessary requestMatcher

--- src/main/java/com/bandomatteo/WebApp/config/SecurityConfiguration.java
+++ src/main/java/com/bandomatteo/WebApp/config/SecurityConfiguration.java
@@ -28,7 +28,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/auth/**","/chat")
+                .requestMatchers("/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
